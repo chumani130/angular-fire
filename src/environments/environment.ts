603284bf1013b9138874cbae9767a34b3ce5af43
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-08-30 15:59:44 +0200

    deleted app component

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-fireman-205d1',
+    appId: '1:727642812626:web:c40db4534b8afb3a857d0b',
+    storageBucket: 'angular-fireman-205d1.appspot.com',
+    apiKey: 'AIzaSyDYgtmKoZmqHzaw0gFqLlK8xZ43FoS5H58',
+    authDomain: 'angular-fireman-205d1.firebaseapp.com',
+    messagingSenderId: '727642812626',
+    measurementId: 'G-5J2HSZBBG8',
+  },};
